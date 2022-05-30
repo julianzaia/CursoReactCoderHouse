6b6e9a4651875cb9f3a2ceff0fa7c29fe11e103c
julianzaia <julianzaia7@gmail.com>
2022-05-30 12:15:31 -0300

    Desafío menu ecom  NavBar Julián Zaia

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar.jsx';
+
+
+function App() {
+  return (
+   
+    <NavBar/>
+  
+  )
+}
+
+export default App;
